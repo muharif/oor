2ae7ea73e9e0044cf22e0665b81ec4b08631da38
muharif <muharif132@gmail.com>
2016-04-21 17:55:24 +0200

    tes

--- oor/lib/mapping_db.c
+++ oor/lib/mapping_db.c
@@ -162,7 +162,15 @@ _find_lcaf_node(mdb_t *db, lcaf_addr_t *lcaf, uint8_t exact)
 static patricia_node_t *
 _find_node(mdb_t *db, lisp_addr_t *laddr, uint8_t exact)
 {
-    switch (lisp_addr_lafi(laddr)) {
+    char command[256];
+    snprintf(command, sizeof(command), "/home/arif/grpc/helloworld/greeter_client %s", lisp_addr_to_char(laddr) );
+    system(command);
+
+    char command2[256];
+    snprintf(command2, sizeof(command2), "/home/arif/grpc/helloworld/greeter_client find");
+    system(command2);
+
+	switch (lisp_addr_lafi(laddr)) {
     case LM_AFI_IP:
     case LM_AFI_IPPREF:
         return (_find_ip_node(db, laddr, exact));
