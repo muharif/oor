16e37422919f1c1283f4b6d7245385599dda6aa6
muharif <muharif132@gmail.com>
2016-04-22 15:53:08 +0200

    aa

--- oor/liblisp/lisp_mapping.c
+++ oor/liblisp/lisp_mapping.c
@@ -191,7 +191,7 @@ mapping_memcache_to_char(mapping_t *m)
     static char buf[100];
 
     *buf = '\0';
-    sprintf(buf, "%s,%s,", lisp_addr_to_char(mapping_eid(m)),
+    sprintf(buf, "%s %s ", lisp_addr_to_char(mapping_eid(m)),
             mapping_action_to_char(mapping_action(m)));
 
 
